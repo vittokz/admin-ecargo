d89f6c1d922a17c0bfc4a64f8973d1133b930e31
Nelson Garcia <ngarcia@econtainers.co>
2022-09-21 09:15:27 -0500

    import mattable

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {MatTableModule} from '@angular/material/table';
 //import material-design
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, MatIconModule],
-    exports: [CommonModule, FormsModule, ReactiveFormsModule,MatIconModule],
+    imports: [CommonModule, FormsModule, MatTableModule, ReactiveFormsModule, MatIconModule],
+    exports: [CommonModule, FormsModule, MatTableModule, ReactiveFormsModule,MatIconModule],
 })
 export class SharedModule {}
