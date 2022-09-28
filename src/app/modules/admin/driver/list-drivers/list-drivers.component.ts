06d1b70c4f22423b9546ed4afb087954a3d68c91
Nelson Garcia <ngarcia@econtainers.co>
2022-09-28 17:38:58 -0500

    filtro buscar

--- src/app/modules/admin/driver/list-drivers/list-drivers.component.ts
+++ src/app/modules/admin/driver/list-drivers/list-drivers.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Drivers, Profile } from 'app/shared/model/driver.model';
 import { DriversFirebaseService } from 'app/shared/services/drivers-firebase.service';
@@ -153,4 +154,15 @@ export class ListDriversComponent {
   }
   
   @ViewChild (MatPaginator) paginador: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
